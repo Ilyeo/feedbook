b19c90bc0cdcaa019c1b42ebd0ca3e0a5e5e2094
Ilyeo <jrag.tkd@gmail.com>
2018-04-19 13:52:40 -0500

    Add specs to skill model

--- app/models/feedbook/skill.rb
+++ app/models/feedbook/skill.rb
@@ -2,5 +2,7 @@ module Feedbook
   class Skill < ApplicationRecord
     belongs_to :department
     belongs_to :skill_group
+
+    validates :name, presence: true
   end
 end
--- spec/models/feedbook/skill_spec.rb
+++ spec/models/feedbook/skill_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
-module Feedbook
-  RSpec.describe Skill, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Feedbook::Skill, type: :model do
+  it { should respond_to(:name) }
+  it { should respond_to(:department) }
+  it { should respond_to(:skill_group) }
+
+  describe "association's model" do
+    it { should belong_to(:department) }
+    it { should belong_to(:skill_group) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
   end
 end
