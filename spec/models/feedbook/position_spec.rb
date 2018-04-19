d59b60b4ee0bf70d142cec18e5ab8cbcddcd227a
Diego Mendoza <diego_saul_402@hotmail.com>
2018-04-19 16:03:30 -0500

    Add newline to deparment y position files and change test description

--- spec/models/feedbook/position_spec.rb
+++ spec/models/feedbook/position_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Feedbook::Position, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:job_description) }
   it { should belong_to(:department) }
-  it 'should create a position by factory method' do
+  it 'creates a position by factory method' do
     create(:position)
     expect(Feedbook::Position.last.name).to eq 'CEO'
   end
