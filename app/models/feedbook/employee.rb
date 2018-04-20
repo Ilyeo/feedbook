2d43c0bedb92ab8f1ec423c01870baea373b8e57
Ilyeo <jrag.tkd@gmail.com>
2018-04-20 13:28:09 -0500

    Add association's in Employee model

--- app/models/feedbook/employee.rb
+++ app/models/feedbook/employee.rb
@@ -1,6 +1,8 @@
 module Feedbook
   class Employee < ApplicationRecord
     has_ancestry
+    has_many :employee_has_skills
+    has_many :skills, through: :employee_has_skills
     validates :email, presence: true
     has_many :position_has_employees
   end
--- spec/models/feedbook/employee_spec.rb
+++ spec/models/feedbook/employee_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Feedbook::Employee do
   it { should respond_to(:email) }
   it { should validate_presence_of(:email) }
   it { should respond_to(:ancestry) }
+
+  describe "association's model" do
+    it { should have_many(:employee_has_skills) }
+    it { should have_many(:skills).through(:employee_has_skills) }
+  end
 end
