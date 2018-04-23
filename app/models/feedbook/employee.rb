9036c8c8e56865412cc81ddc2921b35579cc0a9a
Ilyeo <jrag.tkd@gmail.com>
2018-04-23 01:31:33 -0500

    Add Skill associations model with EmployeeHasSkill model

--- app/models/feedbook/employee.rb
+++ app/models/feedbook/employee.rb
@@ -1,9 +1,11 @@
 module Feedbook
   class Employee < ApplicationRecord
     has_ancestry
+    
     has_many :employee_has_skills
     has_many :skills, through: :employee_has_skills
-    validates :email, presence: true
     has_many :position_has_employees
+
+    validates :email, presence: true
   end
 end
