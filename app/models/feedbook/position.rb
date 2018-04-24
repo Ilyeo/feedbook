5bf15c0dcc9224382914f107ab2366228d22c2a9
Ilyeo <jrag.tkd@gmail.com>
2018-04-23 21:07:59 -0500

    Add association between Position model and SkillHasPoosition model

--- app/models/feedbook/position.rb
+++ app/models/feedbook/position.rb
@@ -3,5 +3,7 @@ module Feedbook
     validates :name, :job_description, presence: true
     belongs_to :department
     has_many :position_has_employee
+    has_many :skill_has_positions
+    has_many :skills, through: :skill_has_positions
   end
 end
--- spec/models/feedbook/position_spec.rb
+++ spec/models/feedbook/position_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Feedbook::Position, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:job_description) }
-  it { should belong_to(:department) }
+  it { should respond_to(:name) }
+  it { should respond_to(:job_description) }
+  it { should respond_to(:department) }
+  it { should respond_to(:skill_has_positions) }
+  it { should respond_to(:skills) }
+
+  describe "association's model" do
+    it { should belong_to(:department) }
+    it { should have_many(:skill_has_positions) }
+    it { should have_many(:skills).through(:skill_has_positions) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:job_description) }
+  end
+
   it 'creates a position by factory method' do
     create(:position)
     expect(Feedbook::Position.last.name).to eq 'CEO'
