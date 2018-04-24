1af923eea5d1857f26e824e9a64d99226dd901cb
Ilyeo <jrag.tkd@gmail.com>
2018-04-23 21:17:03 -0500

    Add association between Skill model and SkillHasPosition model

--- app/models/feedbook/skill.rb
+++ app/models/feedbook/skill.rb
@@ -1,10 +1,11 @@
 module Feedbook
   class Skill < ApplicationRecord
-
     belongs_to :department
     belongs_to :skill_group
     has_many :employee_has_skills
     has_many :employees, through: :employee_has_skills
+    has_many :skill_has_positions
+    has_many :positions, through: :skill_has_positions
 
     validates :name, presence: true
   end
--- spec/models/feedbook/skill_spec.rb
+++ spec/models/feedbook/skill_spec.rb
@@ -4,12 +4,16 @@ RSpec.describe Feedbook::Skill, type: :model do
   it { should respond_to(:name) }
   it { should respond_to(:department) }
   it { should respond_to(:skill_group) }
+  it { should respond_to(:skill_has_positions) }
+  it { should respond_to(:positions) }
 
   describe "association's model" do
     it { should belong_to(:department) }
     it { should belong_to(:skill_group) }
     it { should have_many(:employee_has_skills) }
     it { should have_many(:employees).through(:employee_has_skills) }
+    it { should have_many(:skill_has_positions) }
+    it { should have_many(:positions).through(:skill_has_positions) }
   end
 
   describe "validations" do
